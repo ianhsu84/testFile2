b465269852625732c2e61e65e02b0f5074bab55f
ianhsu84 <111257953+ianhsu84@users.noreply.github.com>
2022-08-16 10:41:13 +0800

    case_1

--- func.py
+++ func.py
@@ -5,7 +5,23 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 def driver_wait(webdriver,wait_sec,target_id):
-    WebDriverWait(webdriver,wait_sec).until(expected_conditions.presence_of_element_located(By.ID,target_id))
+    WebDriverWait(webdriver,wait_sec).until(expected_conditions.presence_of_element_located((By.ID,target_id)))
 
 def open_webdriver(url):
     return webdriver.Chrome().get(url)
+
+## find element here
+def find_element_by_id(webdriver,target_id):
+    return webdriver.find_element(By.ID,target_id)
+
+def find_element_by_class_name(webdriver,target_class_name):
+    return webdriver.find_element(By.CLASS_NAME,target_class_name)
+
+def find_element_by_linktext(webdriver,target_linktext):
+    return webdriver.find_element(By.LINK_TEXT,target_linktext)
+
+def find_element_by_css_selector(webdriver,target_css_selector):
+    return webdriver.find_element(By.CSS_SELECTOR,target_css_selector)
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from func import *
-
+from case_1 import *
 url = 'https://www.vpin.club/main?sort=date'
 wd = webdriver.Chrome()
 wd.get(url)
 
+driver_wait(wd,30,'main-style')
+
+print('OK')
+wd.quit()
